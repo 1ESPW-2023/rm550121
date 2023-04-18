b7a6ca6efa813785f2b165b17194bfa748af30a2
Gabriel-Machado-Belardino <rm550121@fiap.com.br>
2023-04-18 20:16:55 -0300

    Working with querySelector/map/indexOf

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-const divElement = document.getElementById('div-box')
+// const divElement = document.getElementById('div-box')
 
-divElement.addEventListener("click", mostraElemento)
+// divElement.addEventListener("click", mostraElemento)
 
-function mostraElemento() {
-    console.log(divElement)
-}
+// function mostraElemento() {
+//     console.log(divElement)
+// }
 
-function soma()
-{
-    let n1 = 10
-    let n2 = 5
-    console.log(n1 + n2)
-}
+// function soma()
+// {
+//     let n1 = 10
+//     let n2 = 5
+//     console.log(n1 + n2)
+// }
+arrayLiElements = [...document.querySelectorAll('li')];
+
+arrayLiElements.map((e,i)=>{
+    if(e.textContent.indexOf('1') != (-1))
+    {
+        e.textContent = '[ ' + e.textContent + ' ]'
+    }
+});
+
+// console.log(arrayLiElements);
