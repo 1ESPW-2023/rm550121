f1e0240c4fbcafd5dc1db8f287937013e882ba66
Gabriel-Machado-Belardino <rm550121@fiap.com.br>
2023-04-18 21:00:30 -0300

    Estruturando exemplo-cores.html/Extilizando HTML/Trocando a cor do conteudo com setTimeout

--- js/script.js
+++ js/script.js
@@ -12,13 +12,32 @@
 //     let n2 = 5
 //     console.log(n1 + n2)
 // }
-arrayLiElements = [...document.querySelectorAll('li')];
+// arrayLiElements = [...document.querySelectorAll('li')];
 
-arrayLiElements.map((e,i)=>{
-    if(e.textContent.indexOf('1') != (-1))
-    {
-        e.textContent = '[ ' + e.textContent + ' ]'
-    }
-});
+// arrayLiElements.map((e,i)=>{
+//     if(e.textContent.indexOf('1') != (-1))
+//     {
+//         e.textContent = '[ ' + e.textContent + ' ]'
+//     }
+// });
 
 // console.log(arrayLiElements);
+
+let tmp = 0
+
+function mudaCor()
+{
+    let r = Math.ceil(Math.random()*255);
+    let g = Math.ceil(Math.random()*255);
+    let b = Math.ceil(Math.random()*255);
+    const elemento = [...document.getElementsByClassName("conteudo")][0]
+    elemento.style.backgroundColor = `rgb(${r},${g},${b})`
+
+    tmp = setTimeout(mudaCor, 100);
+}
+mudaCor()
+
+function pararTimeout()
+{
+    clearTimeout(tmp);
+}
